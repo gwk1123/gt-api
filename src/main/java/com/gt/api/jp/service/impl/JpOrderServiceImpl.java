49a2e98fafdd9892d215a6485f5b1fbe5cbea96e
guowenke <guowenke@hongthai.com>
2021-10-15 17:55:39 +0800

    s2

---
+++ src/main/java/com/gt/api/jp/service/impl/JpOrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.gt.api.jp.service.impl;
+
+import com.gt.api.jp.service.JpOrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpOrderServiceImpl implements JpOrderService {
+}
